3a5ada5b94ff8c85e8b4e7e51049009e87030d9b
Jagath Jai Kumar <jagath.jaikumar@gmail.com>
2021-12-22 16:43:03 -0500

    sentence similarity example

---
+++ sentence_similarity_example.py
@@ -0,0 +1,16 @@
+import spacy
+
+# pip install spacy
+# python -m spacy download en_core_web_lg
+nlp = spacy.load("en_core_web_lg")
+# nlp = spacy.load("en_core_web_md")
+
+
+doc1 = nlp(u"the person wear red T-shirt")
+doc2 = nlp(u"this person is walking")
+doc3 = nlp(u"the boy wear red T-shirt")
+
+
+print(doc1.similarity(doc2))
+print(doc1.similarity(doc3))
+print(doc2.similarity(doc3))
